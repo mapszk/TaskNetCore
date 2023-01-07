f5fa8a90d215cf4985009321b0fb8728d8484e3d
Martin Pilaszek <martinpilaszek@gmail.com>
2023-01-06 21:35:35 -0300

    added startup

---
+++ Startup.cs
@@ -0,0 +1,32 @@
+public class Startup
+{
+    private readonly IConfiguration config;
+
+    public Startup(IConfiguration config)
+    {
+        this.config = config;
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+    }
+
+    public void Configure(WebApplication app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+    }
+}
